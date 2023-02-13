e5e82679f98bc8a8e2532878e92f7e2fbc421feb
Mani Vunnam <naidum206@gmail.com>
2023-02-13 11:40:56 +0530

    firstcommit

---
+++ Account_login/Account_login/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Account_login.Models
+{
+    public class DataContext:IdentityDbContext<UserLogin>
+    {
+        public DataContext(DbContextOptions <DataContext> options):base(options) 
+        {
+
+        }
+        public DbSet<UserLogin> AccountTABLE { get; set; }
+
+
+        
+    }
+}
