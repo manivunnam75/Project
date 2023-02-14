cf1489ec596f19f382e333799346c1226c41708d
manivunnam75 <115546173+manivunnam75@users.noreply.github.com>
2023-02-14 15:03:25 +0530

    Delete Account_login/Account_login directory

--- Account_login/Account_login/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Account_login.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
